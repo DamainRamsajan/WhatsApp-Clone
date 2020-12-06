32aa403903ab46cec1c9ec7ed4e6609fc0f99aaf
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-06 23:03:39 +0100

    WhatsApp Clone Frontend first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyArMjWJMhkbpTxuVy12_qVIC8j2w1uI8z8",
+    authDomain: "whatsapp-clone-d9513.firebaseapp.com",
+    projectId: "whatsapp-clone-d9513",
+    storageBucket: "whatsapp-clone-d9513.appspot.com",
+    messagingSenderId: "489738206045",
+    appId: "1:489738206045:web:a45839ffdf13c790ac86b4",
+    measurementId: "G-4FCWWKBKGS"
+  };
