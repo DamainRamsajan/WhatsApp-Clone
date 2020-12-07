daa4d9e59cdea85f96d19ce38db401d37eaff96a
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-07 03:06:32 +0100

    WhatsApp Clone Frontend 3rd commit

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,29 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { SearchOutlined, AttachFile, MoreVert } from '@material-ui/icons'
 import React from 'react'
 import "./Chat.css"
 
 function Chat() {
     return (
         <div className = "chat">
-            <h1>I am the chat component</h1>
+           <div className = "chat__header">
+               <Avatar />
+               <div className ="chat__headerInfo">
+                   <h3>room name</h3>
+                   <p>last seen at ....</p>
+               </div>
+               <div className ="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>  
+               </div>
+           </div>
         </div>
     )
 }
