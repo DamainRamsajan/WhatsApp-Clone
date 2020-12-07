8c859785761907863bafa8da3a921489f862f3d0
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-07 14:39:31 +0100

    WhatsApp Clone rough complete v1

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,6 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { SearchOutlined, AttachFile, MoreVert } from '@material-ui/icons'
+import { SearchOutlined, AttachFile, MoreVert, InsertEmoticon } from '@material-ui/icons'
+import MicIcon from "@material-ui/icons/Mic"
 import React from 'react'
 import "./Chat.css"
 
@@ -24,6 +25,63 @@ function Chat() {
                     </IconButton>  
                </div>
            </div>
+           <div className ="chat__body">
+               <p className = "chat__message">
+                   <span className = "chat__name">Damian
+                   </span>
+                   this is a message
+                   
+                   <span className = "chat__timestamp">
+                       {new Date().toUTCString()}
+                    </span>
+               </p>
+
+               <p className = "chat__message chat__reciever">
+                   <span className = "chat__name">Bob Marley
+                   </span>
+                   we gonna chase them crazy bump heads outa yard
+                   
+                   <span className = "chat__timestamp">
+                       {new Date().toUTCString()}
+                    </span>
+               </p>  
+
+               <p className = "chat__message">
+                   <span className = "chat__name">Damian
+                   </span>
+                   this is a message
+                   
+                   <span className = "chat__timestamp">
+                       {new Date().toUTCString()}
+                    </span>
+               </p>
+
+               <p className = "chat__message chat__reciever">
+                   <span className = "chat__name">Bob Marley
+                   </span>
+                   we be jammin
+                   
+                   <span className = "chat__timestamp">
+                       {new Date().toUTCString()}
+                    </span>
+               </p>  
+           </div>
+
+           <div className ="chat__footer">
+               <InsertEmoticon />
+               <form>
+                   <input 
+                   placeholder ="type a message"
+                   type = "text"
+                   />
+                   <button 
+                   type = "submit">
+                       Send a message
+                   </button>
+               </form>
+               <MicIcon />
+           </div>
+
         </div>
     )
 }
